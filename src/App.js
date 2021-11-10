14a8d4cabf81623adde1222c739fe5709485294a
unknown <cferrara212@gmail.com>
2021-11-10 14:09:17 -0700

    initial react-app commit with basic react app created, unnecessary files deleted, and a coulple of starter component folders created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import SearchBar from './SearchBar/SearchBar';
+import EmbedVideo from './EmbedVideo/EmbedVideo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/EmbedVideo/EmbedVideo.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
+
+const EmbedVideo = (props) =>{
+    pass
+}
+
+export default EmbedVideo;
