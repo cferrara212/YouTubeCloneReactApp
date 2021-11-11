320f9a6a4456b464102c974e5e049fdbc3b4c92e
unknown <yoshuah809@gmail.com>
2021-11-11 16:57:53 -0600

    Uploading API call test via console -in Working condition

--- src/Components/VideoSuggestion/VideoSuggestion.jsx
+++ src/Components/VideoSuggestion/VideoSuggestion.jsx
@@ -1,23 +1,29 @@
 import React, { useState, useEffect, Component } from 'react';
-
 import { ListGroup } from 'react-bootstrap';
+import Youtube from 'simple-youtube-api';
+import { googleAPIKey } from '../../keys';
 
-class VideoSuggestion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <ListGroup>
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-    );
-  }
-}
+const youtube = new Youtube(googleAPIKey);
+
+const VideoSuggestion = props => {
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  const callApi = async () => {
+    const result = await youtube.search('React', 5);
+    console.log(result);
+  };
+
+  return (
+    <ListGroup>
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+  );
+};
 
 export default VideoSuggestion;
