28e06d79da443b9ea2b3011db90ab75a03b2ba83
unknown <cferrara212@gmail.com>
2021-11-11 13:51:24 -0700

    Added in code for embed video file. Did bootstrap install from my side

--- src/EmbedVideo/EmbedVideo.jsx
+++ src/EmbedVideo/EmbedVideo.jsx
@@ -1,9 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
 
+import React, { Component } from 'react';
+import {ListGroup} from 'react-bootstrap'
+
+class VideoSuggestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+        <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup> );
+    }
+}
+ 
+export default VideoSuggestion;
+
+
+
+
 const EmbedVideo = (props) =>{
-    return <h1>The Video goes here</h1>;
-    
+    return (
+    <>
+            <div>  
+                <div> 
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/2hol2sS3ONs" 
+                             title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+                             clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                    </iframe> 
+                 </div>
+                <div> Title </div>
+                <div>  Description </div>
+            </div>
+
+    </>
+                )
 }
 
 export default EmbedVideo;
