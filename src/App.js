9d2a08a982d7db439aeba2327123551a2c904ba1
unknown <yoshuah809@gmail.com>
2021-11-15 10:01:09 -0600

    Modified the search engine and app.js page to use hooks and usestate

--- src/App.js
+++ src/App.js
@@ -1,80 +1,39 @@
+import React, { useState } from 'react';
 import './App.css';
-import axios from 'axios';
-import EmbedVideo from './EmbedVideo/EmbedVideo';
-import SearchBar from './Components/SearchBar/SearchBar';
-import { Container,Col,Row } from 'react-bootstrap';
-import { googleAPIKey } from './keys'
-import React, { useState, useEffect} from 'react';
-import VideoSuggestion from './Components/VideoSuggestion/VideoSuggestion';
-import VideoList from './Components/VideoSuggestion/VideoList';
+import Search from './Components/Search/Search';
+import youtubeApi from './Components/Api//Youtube';
+import VideoList from './Components/VideoList/VideoList';
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer';
 
-function App() {
-  // this.state = {
-  //   videoMetadata: [],
-  //   selectedVideId: null
-  // };
+const App = () => {
+  const [videosMetaInfo, setVideosMetaInfo] = useState([]);
+  const [selectedVideoId, setSelectedVideoId] = useState(null);
 
-  axios.defaults.baseURL = 'https://www.googleapis.com/youtube/v3/';
-
-  const [suggested, setSuggested] = useState();
-
-  useEffect(() => {
-    callApi();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const callApi = async () => {
-    try {
-      let result = await axios.get(
-        `search?maxResults=5&part=snippet&key=${googleAPIKey}&type=video`
-      );
-      //console.log(result.data.items);
-      setVideos(result.data);
-    } catch (ex) {
-      console.log('error in API call');
-    }
-  };
-  function setVideos(data) {
-    setSuggested(data);
-  }
-  //snippet . thumbnails . default . url  for video id, element.id
-
-  const search = async searchTerm => {
-    try {
-      let result = await axios.get(
-        `search?q=${searchTerm}&maxResults=10&part=snippet&key=${googleAPIKey}&type=video`
-      );
-      console.log(result.data);
-      setSuggested(result.data);
-    } catch (ex) {
-      console.log('error in API call');
-    }
+  const onVideoSelected = videoId => {
+    setSelectedVideoId(videoId);
   };
 
-  //snippet . thumbnails . default . url  for video id, element.id
+  const onSearch = async keyword => {
+    const response = await youtubeApi.get('/search', {
+      params: {
+        q: keyword,
+      },
+    });
+    //console.log(response)
+    setVideosMetaInfo(
+      response.data.items,
+      setSelectedVideoId(response.data.items[0].id.videoId)
+    );
+    //console.log(this.state);
+  };
+
   return (
-    <Container>
-      <Row>
-        <Col style={{ border: '2px solid blue' }}>
-          <SearchBar search={search} />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} lg={8} style={{ border: '2px solid blue' }}>
-          <EmbedVideo />
-        </Col>
-        <Col xs={12} lg={4} style={{ border: '2px solid blue' }}>
-          {/* <VideoSuggestion suggested={suggested} /> */}
-          <VideoList suggested={suggested} />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} lg={8} style={{ border: '2px solid blue' }}>
-          Messages
-        </Col>
-      </Row>
-    </Container>
+    <div className='App'>
+      <Search onSearch={onSearch} />
+      <VideoList onVideoSelected={onVideoSelected} data={videosMetaInfo} />
+      <VideoPlayer videoId={selectedVideoId} />
+    </div>
   );
-}
+};
 
 export default App;
